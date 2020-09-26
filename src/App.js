7e1b3bf30862a0d620a0673c1b115060366733d1
Gor <gor.sharoyan95@gmail.com>
2020-09-26 19:54:05 +0400

    fixed bugs with incremetn/decrement functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import store,{ decrement, increment } from './store/index';
 
 
 function App() {
+ const {counter}=useSelector((store)=>{
+   return store;
+ })
+
+ const dispatch=useDispatch();
+ 
+ const handleInc=()=>{
+    dispatch(increment());
+    console.log('INCREMENT')
+ }
+
+const handleDec=()=>{
+   dispatch(decrement());
+   console.log('DECREMENT')
+ }
+
+
   return (
     <div className="App">
-
+      <header>
+        <h1>Counter</h1>
+        <p>Count:{counter}</p>
+        <button onClick={handleInc}>+</button>
+        <button onClick={handleDec}>-</button>
+      </header>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import {configureStore,createAction,createReducer} from "@reduxjs/toolkit";
+
 let initialState={};
 export const increment=createAction('ICREMENT');
 export const decrement= createAction('DECREMENT');
 
 const counter =createReducer(0,{
-    [increment.type]:(store)=>store+1,
-    [decrement.type]:(store)=>store-1
+    [increment.type]:(state)=>state+1,
+    [decrement.type]:(state)=>state-1,
 })
 
-const store =configureStore({
-    reducer:counter,
-};
+const store =configureStore(
+{
+    reducer:{counter}
+},
 initialState
 )
+
+export default store
